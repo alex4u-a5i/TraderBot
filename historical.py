f986d79da1f41eb41bedbe8ebcc3db4a6af93622
Alexandru <alex_apetrei@outlook.com>
2021-03-01 13:50:03 +0000

    A way to fetch historical data for backtesting

--- historical.py
+++ historical.py
@@ -2,11 +2,29 @@ from binance.client import Client
 from binance.helpers import date_to_milliseconds
 import json
 
+""" Kline Format e.g.
+[
+    1499040000000,  # Open time
+    "0.01634790",  # Open
+    "0.80000000",  # High
+    "0.01575800",  # Low
+    "0.01577100",  # Close
+    "148976.11427815",  # Volume
+    1499644799999,  # Close time
+    "2434.19055334",  # Quote asset volume
+    308,  # Number of trades
+    "1756.87402397",  # Taker buy base asset volume
+    "28.46694368",  # Taker buy quote asset volume
+    "17928899.62484339"  # Ignore
+]"""
 
 class HistoricalKlines:
+    """Fetch Historical Klines from Binance"""
     client = Client("", "")
 
-    def __init__(self, symbol, interval, start_str, klines=[[]]):
+    def __init__(self, symbol, interval, start_str, klines=None):
+        if klines is None:
+            klines = [[]]
         self.symbol = symbol
         self.klines = klines
         self.interval = interval
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# A crypto trading bot for binance
 from historical import HistoricalKlines
 from binance.client import Client
 
@@ -6,6 +7,8 @@ if __name__ == '__main__':
     eth = HistoricalKlines("ETHUSDT", Client.KLINE_INTERVAL_4HOUR, "25 Feb, 2021")
     eth.fetch_klines("26 Feb, 2021")
     eth.fetch_klines("27 Feb, 2021")
+    eth.save_to_file()
+
 
 
 
